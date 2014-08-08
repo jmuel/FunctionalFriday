62037f0cae8adde446dd0e2c316c00924946e66b
jmuel <jmuel11@gmail.com>
2014-08-08 13:10:38 -0500

    added the hanoi problem

---
+++ hanoi.js
@@ -0,0 +1,15 @@
+var hanoi = function(num, a, b, c) {
+    if(num > 1) hanoi(num-1, a, c, b);
+    console.log("move the top disk from ", a, " to ", b);
+    if(num > 1) hanoi(num-1, c, b, a);
+}
+
+var solveHanoi = function(num, a, b, c) {
+    a = typeof a !== 'undefined' ? a : 1;
+    b = typeof b !== 'undefined' ? b : 2;
+    c = typeof c !== 'undefined' ? c : 3;
+
+    return hanoi(num, a, b, c);
+}
+
+solveHanoi(2, "A", "C", "B")
