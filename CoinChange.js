e4bd37132d89d3aba1374490729888cf23e15419
jmuel <jmuel11@gmail.com>
2014-08-15 13:23:08 -0500

    finished CoinChange

--- CoinChange.js
+++ CoinChange.js
@@ -1,14 +1,15 @@
 var _ = require("underscore");
 
-var countChange = _.memoize(function(current, coins) {
-    if(current == 0) return 1;
-    if(current < 0) return 0;
-    return _.reduce(coins, function(memo, coin) {
-        return memo + countChange(current - coin, _.filter(coins, function(num) {return num <= coin}));
-    }, 0);
+var countChange = _.memoize(
+    function(current, coins) {
+        if(current == 0) return 1;
+        if(current < 0) return 0;
+        return _.reduce(coins, function(memo, coin) {
+            return memo + countChange(current - coin, _.filter(coins, function(num) {return num <= coin}));
+        }, 0);
 
     }, function(current, coins) {
         return current + coins + "";
     });
 
-console.log(countChange(200, [50, 25, 10, 5, 1]));
+console.log(countChange(100, [50, 25, 10, 5, 1]));
